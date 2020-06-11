4bd830248abc949678122648ffa224269aab5290
abhimamk <abhimamk8@gmail.com>
2020-06-11 11:47:16 +0530

    auto login and token login

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,114 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject, BehaviorSubject } from 'rxjs';
+import { User } from './user.model';
+import { Router } from '@angular/router';
+export interface AuthRequestData {
+  Kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user = new BehaviorSubject<User>(null);
+  constructor(private http: HttpClient, private router: Router) { }
+  signup(email: string, password: string) {
+    return this.http.post<AuthRequestData>(
+
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBIVroyGam2ENGCNXwYW5iH4BvAoBgEcNg',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(catchError(this.handleError), tap(responseData => {
+      this.handleAuthentication(
+        responseData.email,
+        responseData.localId,
+        responseData.idToken,
+        +responseData.expiresIn
+      );
+    }
+    ));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthRequestData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBIVroyGam2ENGCNXwYW5iH4BvAoBgEcNg',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(catchError(this.handleError), tap(responseData => {
+      this.handleAuthentication(
+        responseData.email,
+        responseData.localId,
+        responseData.idToken,
+        +responseData.expiresIn
+      );
+    })
+    );
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate);
+    this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
+  }
+// save user login detail in localStorage , if user refresh the screen it is not log out
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
+  }
+  private handleError(errorresponse: HttpErrorResponse) {
+    let errorMessageResponce = 'An unknown error occurred!';
+    if (!errorresponse.error || !errorresponse.error.error) {
+      return throwError(errorMessageResponce);
+    }
+    switch (errorresponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessageResponce = 'This email already exists';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessageResponce = 'Email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessageResponce = 'this password is not correct';
+        break;
+    }
+    return throwError(errorMessageResponce);
+  }
+}
---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+  constructor(
+    public email: string,
+    public id: string,
+    private _token: string,
+    private __tokenExpirationDate: Date
+  ) { }
+  get token() {
+    if (!this.__tokenExpirationDate || new Date() > this.__tokenExpirationDate) {
+      return null;
+    }
+    return this._token;
+  }
+}
