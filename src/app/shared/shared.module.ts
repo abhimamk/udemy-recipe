b3a8c9a08b7ad4b8433fc9214c6161334d198828
abhimamk <abhimamk8@gmail.com>
2020-06-14 14:13:50 +0530

    shared and shopping mosule

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { AlertComponent } from './alert/alert.component';
+import { LoadingComponent } from './loading/loading.component';
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
+import { DropdownDirective } from './dropdown.directive';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    LoadingComponent,
+    PlaceholderDirective,
+    DropdownDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AlertComponent,
+    LoadingComponent,
+    PlaceholderDirective,
+    DropdownDirective,
+    CommonModule
+  ],
+  entryComponents: [AlertComponent]
+})
+export class SharedModule { }
